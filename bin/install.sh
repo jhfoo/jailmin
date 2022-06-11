b2f3c1cffc40baee03c1db5f8b883ce1b64f5e49
Foo JH <foojihaw@hotmail.com>
2022-06-10 22:58:01 -0500

    NEW: working cmd state -> priority

--- bin/install.sh
+++ bin/install.sh
@@ -1,14 +1,43 @@
-echo - Create local bin/ if not exist
+#!/usr/local/bin/zsh
+
+TEXT_OPENING="
+*** ASSUMPTIONS
+*** - You are not using a root account
+*** - sudo is an installed package
+"
+
+TEXT_CLOSING="
+*** This install script only makes it easier for jailmin to be run.
+*** DO NOT remove the jailmin Git folder.
+*** Your config files should be placed in /usr/local/etc/jailmin/
+"
+
+RELEASE_LATEST='13.1-RELEASE'
+
+echo $TEXT_OPENING
+
+echo "- Create local bin/ if not exist"
 [ -d ~/bin ] || mkdir ~/bin
 
-echo - Linking executing to local bin/
+echo "- Create /usr/local/etc/jailmin/ if not exist"
+[ -d /usr/local/etc/jailmin ] || sudo mkdir /usr/local/etc/jailmin
+
+echo "- Linking executing to local bin/"
 rm ~/bin/jailmin
 ln -s "$(pwd)/bin/jailmin.py" ~/bin/jailmin
 
-echo - Install required packages
-sudo pkg install -y py38-iocage py38-yaml
+echo "- Install external dependencies"
+sudo pkg install -y py38-iocage py38-pip
 
-echo - Fetch latest RELEASE base files
-sudo iocage fetch -r LATEST
+echo "- Install Python dependencies"
+sudo pip install -t /usr/local/etc/jailmin/packages -r requirements.txt 
+
+printf '- Fetch latest RELEASE base files? y/N '
+read YESNO
+# read YESNO\?"Fetch latest RELEASE base files?"
+[[ $YESNO == 'y' ]] && sudo iocage fetch -r $RELEASE_LATEST
+
 #echo -e "alias siocage='sudo iocage'" >> ~/.zshrc
 #alias siocage='sudo iocage'
+
+echo $TEXT_CLOSING
--- bin/jailmin.py
+++ bin/jailmin.py
@@ -1,3 +1,38 @@
 #!/usr/local/bin/python3.8
+# core modules
+from ast import parse
+from curses import has_key
+from importlib.resources import path
+import os
+import sys
+import pathlib
 
-print ('hello!')
+def appendLibPath():
+  # Assumes lib/ is relative to actual jailmin.py, not the soft link
+  # in future this should be moved to /usr/local/bin/jailminlib/
+  BinFullFilename = __file__
+  if os.path.islink(BinFullFilename):
+    BinFullFilename = os.readlink(BinFullFilename)
+  AppPath = str(pathlib.Path(os.path.join(os.path.dirname(BinFullFilename), '..')).resolve())
+  sys.path.append(AppPath)
+
+  sys.path.append('/usr/local/etc/jailmin/packages')
+
+appendLibPath()
+
+# custom
+import lib.logger as logger
+import lib.init as init
+import lib.IocageAdapter as IocageAdapter
+import lib.CliParser as CliParser
+import lib.statebuilder as statebuilder
+
+JailManager = IocageAdapter.IocageAdapter ()
+
+logger.onlyConsole()
+init.validateRoot()
+
+CurrentJailState = statebuilder.getCurrentState(JailManager)
+CliParser.execCli(CliParser.parseCli(), JailManager, CurrentJailState)
+
+
