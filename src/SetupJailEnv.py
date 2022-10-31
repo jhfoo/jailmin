34b47ff2c8fb724fd2923b6c47af5fe2a9c51caa
Foo <jhfoo@deadend.lan>
2022-10-31 01:34:23 -0500

    pf.conf init

--- src/SetupJailEnv.py
+++ src/SetupJailEnv.py
@@ -1,4 +1,5 @@
 # core lib
+import re
 import subprocess
 
 # public lib
@@ -7,8 +8,8 @@ import psutil
 
 
 
-def getOptionInput(text, default = '', IsYesNo = True, IsYesDefault = False, IsNoDefault = False):
-  if default == '':
+def getOptionInput(text, default = None, IsYesNo = True, IsYesDefault = False, IsNoDefault = False, choices = None):
+  if default == None:
     if IsYesNo:
       if IsYesDefault:
         OptionStr = 'Y/n'
@@ -22,6 +23,7 @@ def getOptionInput(text, default = '', IsYesNo = True, IsYesDefault = False, IsN
   while True:
     resp = input (text + ' [{}]'.format(OptionStr)).lower()
     if resp == '':
+      # no selection: use default if set
       if IsYesDefault:
         resp = 'y'
         break
@@ -32,7 +34,14 @@ def getOptionInput(text, default = '', IsYesNo = True, IsYesDefault = False, IsN
         resp = default
         break
     else:
-      if default != '':
+      # custom input
+      # must choose from list
+      if not choices == None:
+        if resp in choices:
+          return resp
+        else:
+          print ('Options: {}'.format(choices))
+      elif default != None:
         return resp
       elif IsYesNo:
         if resp == 'y' or resp == 'n':
@@ -52,6 +61,33 @@ def execShell(cmd):
     if line == '' and PollResult is not None:
       break
 
+def updatePfConf(BridgeSubnet, InternetInterface):
+  infile = open('/etc/pf.conf', 'r')
+  lines = [
+    '# JAILMIN START: do not edit',
+    'WANIF = "{}"'.format(InternetInterface),
+    'JAILNET = "{}"'.format(BridgeSubnet),
+    'nat on $WANIF inet from { $JAILNET } to any -> {$WANIF}',
+    '# JAILMIN END: do not edit',
+  ]
+
+  isSkipLine = False
+  for line in infile.readlines():
+    if re.search('# JAILMIN START', line):
+      isSkipLine = True
+    elif re.search('# JAILMIN END', line):
+      isSkipLine = False
+    elif not isSkipLine:    
+      lines.append(line.strip())
+  infile.close()
+
+  outfile = open('/etc/pf.conf','w')
+  for line in lines:
+    outfile.write(line + '\n')
+  outfile.close()
+
+  execShell('pfctl -f /etc/pf.conf')
+
 def do():
   print ('Interfaces: ', end='')
   interfaces = []
@@ -66,7 +102,10 @@ def do():
   resp = getOptionInput('Update rc.conf?', IsYesDefault=True)
   if resp == 'y':
     BridgeAddress = getOptionInput('- Bridge address?', default='192.168.100.1/24')
+    BridgeSubnet = getOptionInput('- Bridge subnet?', default='192.168.100.0/24')
+    InternetInterface = getOptionInput('- Internet interface?', default=interfaces[0], choices=interfaces)
     execShell('./bin/install-rc.conf {}'.format(BridgeAddress))
+    updatePfConf(BridgeSubnet = BridgeSubnet, InternetInterface = InternetInterface)
 
   # resp = getOptionInput('Restart network?', IsYesDefault=True)
   # if resp == 'y':
